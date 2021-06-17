b22e18b646be67a154577de170aed8325b2140ff
furkangurel23 <gurel.furkan23@gmail.com>
2021-06-17 22:23:11 +0300

    scheduled task

--- com.sicaklik.takip.bsf/src/main/java/com/sicaklik/takip/com/sicaklik/takip/bsf/ScheduledTasks.java
+++ com.sicaklik.takip.bsf/src/main/java/com/sicaklik/takip/com/sicaklik/takip/bsf/ScheduledTasks.java
@@ -2,10 +2,12 @@ package com.sicaklik.takip.com.sicaklik.takip.bsf;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -31,6 +33,53 @@ public class ScheduledTasks {
 	@Autowired
 	private CihazSicaklikServiceImp cihazSicaklikService;
 	
+	
+	// Read last line of the file
+	private String readFromLast(File file){  
+		StringBuilder builder = new StringBuilder();
+		RandomAccessFile randomAccessFile = null;
+		try {
+			randomAccessFile = new RandomAccessFile(file, "r");
+			long fileLength = file.length() - 1;
+			// Set the pointer at the last of the file
+			randomAccessFile.seek(fileLength);
+			for(long pointer = fileLength; pointer >= 0; pointer--){
+				randomAccessFile.seek(pointer);
+				char c;
+		        // read from the last one char at the time
+		        c = (char)randomAccessFile.read(); 
+		        // break when end of the line
+		        if(c == '\n'){
+		        	break;
+		        }
+		        builder.append(c);
+			}
+			// Since line is read from the last so it 
+			// is in reverse so use reverse method to make it right
+			builder.reverse();
+			System.out.println("Line - " + builder.toString());
+			return builder.toString();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(randomAccessFile != null){
+				try {
+					randomAccessFile.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+	
+	
 //		 "0 0 * * * *" = the top of every hour of every day.
 //		 "*/10 * * * * *" = every ten seconds.
 //		 "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
@@ -48,53 +97,67 @@ public class ScheduledTasks {
 	@Scheduled(cron = "*/10 * * * * *")	//  --> 06:00
 	public void anlikDegerAl() throws IOException {
 		File file = new File("C:\\Users\\furka\\OneDrive\\Masaüstü\\denemeDeger.txt");
-		FileWriter myWriter = new FileWriter("C:\\Users\\furka\\OneDrive\\Masaüstü\\okunanDeger.txt");
-		
-		BufferedReader br = new BufferedReader(new FileReader(file));
-
-		
-		/*
-		 * Simdilik tek cihaz ustunden calisiyoruz ileride daha dinamik hale getirilebilir
-		 * */
-
+//		FileWriter myWriter = new FileWriter("C:\\Users\\furka\\OneDrive\\Masaüstü\\okunanDeger.txt");
 		Cihaz cihaz = cihazService.getir(1L);
-		
-		String st;
+		String lastLine = readFromLast(file);
+		System.out.println(lastLine);
 		String bagilNem = "";
 		String ortamSicakligi = "";
 		String betonSicakligi = "";
 		CihazSicaklik xs = null;
-		while ((st = br.readLine()) != null) {
-			if(st.startsWith("*"))
-				continue;
-			else {
-				String[] elemans = st.split("\t");
-				if(elemans.length > 1) {
-					bagilNem = elemans[0].substring(elemans[0].indexOf("%")+1, elemans[0].length()).trim();
-					ortamSicakligi = elemans[1].substring(elemans[1].indexOf(":")+1, elemans[1].indexOf("°")).trim();
-					betonSicakligi = elemans[2].substring(elemans[2].indexOf(":")+1, elemans[2].indexOf("°")).trim();
-					System.out.println(Arrays.toString(elemans));
-					System.out.print(bagilNem + " - "+ ortamSicakligi + " - " + betonSicakligi);
-					System.out.println();
-					xs = new CihazSicaklik();
-					xs.setBagil_nem(Double.parseDouble(bagilNem));
-					xs.setSicaklik_hava(Double.parseDouble(ortamSicakligi));
-					xs.setSicaklik_beton(Double.parseDouble(betonSicakligi));
-					xs.setCihaz_id(cihaz);
-					xs.setOlusturma_tarih_saati(new Date());
-					cihazSicaklikService.kaydet(xs);
-				}
-			}
-			myWriter.write(st);
-			myWriter.write("\n");
-	  	}
-		
-		FileOutputStream fooStream = new FileOutputStream(file, false);
-
-		fooStream.write("".getBytes());
-		fooStream.close();
-		
-		myWriter.close();  
+		String[] elemans = lastLine.split("\t");
+		if(elemans.length > 1) {
+			bagilNem = elemans[0].substring(elemans[0].indexOf("%")+1, elemans[0].length()).trim();
+			ortamSicakligi = elemans[1].substring(elemans[1].indexOf(":")+1, elemans[1].indexOf("Â")).trim();
+			betonSicakligi = elemans[2].substring(elemans[2].indexOf(":")+1, elemans[2].indexOf("Â")).trim();
+			System.out.println(Arrays.toString(elemans));
+			System.out.print(bagilNem + " - "+ ortamSicakligi + " - " + betonSicakligi);
+			xs = new CihazSicaklik();
+			xs.setBagil_nem(Double.parseDouble(bagilNem));
+			xs.setSicaklik_hava(Double.parseDouble(ortamSicakligi));
+			xs.setSicaklik_beton(Double.parseDouble(betonSicakligi));
+			xs.setCihaz_id(cihaz);
+			xs.setOlusturma_tarih_saati(new Date());
+			cihazSicaklikService.kaydet(xs);
+		}
+//		BufferedReader br = new BufferedReader(new FileReader(file));
+//		Cihaz cihaz = cihazService.getir(1L);
+//		String st;
+//		String bagilNem = "";
+//		String ortamSicakligi = "";
+//		String betonSicakligi = "";
+//		CihazSicaklik xs = null;
+//		while ((st = br.readLine()) != null) {
+//			if(st.startsWith("*"))
+//				continue;
+//			else {
+//				String[] elemans = st.split("\t");
+//				if(elemans.length > 1) {
+//					bagilNem = elemans[0].substring(elemans[0].indexOf("%")+1, elemans[0].length()).trim();
+//					ortamSicakligi = elemans[1].substring(elemans[1].indexOf(":")+1, elemans[1].indexOf("°")).trim();
+//					betonSicakligi = elemans[2].substring(elemans[2].indexOf(":")+1, elemans[2].indexOf("°")).trim();
+//					System.out.println(Arrays.toString(elemans));
+//					System.out.print(bagilNem + " - "+ ortamSicakligi + " - " + betonSicakligi);
+//					System.out.println();
+//					xs = new CihazSicaklik();
+//					xs.setBagil_nem(Double.parseDouble(bagilNem));
+//					xs.setSicaklik_hava(Double.parseDouble(ortamSicakligi));
+//					xs.setSicaklik_beton(Double.parseDouble(betonSicakligi));
+//					xs.setCihaz_id(cihaz);
+//					xs.setOlusturma_tarih_saati(new Date());
+//					cihazSicaklikService.kaydet(xs);
+//				}
+//			}
+//			myWriter.write(st);
+//			myWriter.write("\n");
+//	  	}
+//		
+//		FileOutputStream fooStream = new FileOutputStream(file, false);
+//
+//		fooStream.write("".getBytes());
+//		fooStream.close();
+//		
+//		myWriter.close();  
 	}
 
 }
